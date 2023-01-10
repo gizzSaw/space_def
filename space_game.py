b4ada55acbc04c301bd395173448af86a39ef929
gizzSaw <gizzsaw@gmail.com>
2023-01-11 01:18:01 +0300

    2

--- space_game.py
+++ space_game.py
@@ -1,22 +1,17 @@
-import pygame
-import sys
+import pygame, controls
 from gun import Gun
 
-def run():
 
+def run():
     pygame.init()
-    screen = pygame.display.set_mode((600, 400))
+    screen = pygame.display.set_mode((600, 700))
     pygame.display.set_caption("Space Defenders")
     bg_color = (110, 10, 10)
     gun = Gun(screen)
-
-    print(gun)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        controls.events(gun)
+        gun.update_gun()
         screen.fill(bg_color)
         gun.output()
         pygame.display.flip()
-
 run()
